190279f2ddb9080a77546c170b3f38496e1fd535
cam0347 <34382793+cam0347@users.noreply.github.com>
2022-07-13 17:32:53 +0200

    Add files via upload

---
+++ test/Main.java
@@ -0,0 +1,17 @@
+package test;
+import ml.*;
+
+@SuppressWarnings("ALL")
+public class Main {
+    public static void main(String[] args) {
+        double[][] data = SupervisedNetwork.loadCSV("/users/camillozavattaro/desktop/dataset/autoencoder.csv", 4)[0];
+        NeuralNetwork nn = new NeuralNetwork(data, data, new NNParameters(4, new int[] {4, 4}, 200, 0.0001, true, new NNActivation[] {NNActivation.LINEAR, NNActivation.LINEAR}, NNError.MAE));
+        nn.train();
+
+        nn.test(new double[][] {{500, 912, -7, -134}, {0, -1000, 12, 56}}, new double[][] {{500, 912, -7, -134}, {0, -1000, 12, 56}});
+
+        for (double d: nn.getAnswer(new double[] {500, 912, -7, -134})) {
+            System.out.println(d);
+        }
+    }
+}
