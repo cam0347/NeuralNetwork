97cb094102322e0d30b8fbfeb129da51244d44ce
cam0347 <34382793+cam0347@users.noreply.github.com>
2022-07-13 17:33:38 +0200

    Update Main.java

--- test/Main.java
+++ test/Main.java
@@ -5,7 +5,7 @@ import ml.*;
 public class Main {
     public static void main(String[] args) {
         double[][] data = SupervisedNetwork.loadCSV("/users/camillozavattaro/desktop/dataset/autoencoder.csv", 4)[0];
-        NeuralNetwork nn = new NeuralNetwork(data, data, new NNParameters(4, new int[] {4, 4}, 200, 0.0001, true, new NNActivation[] {NNActivation.LINEAR, NNActivation.LINEAR}, NNError.MAE));
+        NeuralNetwork nn = new NeuralNetwork(data, data, new NNParameters(4, new int[] {3, 4}, 200, 0.0001, true, new NNActivation[] {NNActivation.LINEAR, NNActivation.LINEAR}, NNError.MAE));
         nn.train();
 
         nn.test(new double[][] {{500, 912, -7, -134}, {0, -1000, 12, 56}}, new double[][] {{500, 912, -7, -134}, {0, -1000, 12, 56}});
